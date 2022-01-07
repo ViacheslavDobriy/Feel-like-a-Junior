e1300eb869e774f28e4701170b0e7f55206eb26e
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-07 23:08:43 +0300

    Добавил решение задачи на выяснение выходной ли сегодня день

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+int GetNumber()
+{
+    Console.WriteLine("Введите номер дня недели: ");
+    int Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
+}
+void Weekend(int number)
+{
+    if(number>0 & number<8)
+    {
+        if(number<6)
+        {
+            Console.WriteLine("Сегодня будний день: ");
+        }
+        else Console.WriteLine("Сегодня выходной");
+    }
+    else Console.WriteLine("Вы ввели некорректный номер дня недели");
+}
+int SomeNumber = GetNumber();
+Weekend(SomeNumber);
