ad2d6f1d80632f84ecf4e1b16a23c15faf631946
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-08 00:17:59 +0300

    Программа выдает сообщение о том, является ли введенное 5 значное число палиндромом

---
+++ Task_5/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+double GetNumber()
+{
+    Console.WriteLine("Введите 5-значное число: ");
+    double Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
+}
+void Palindrom(double number)
+{
+    if(number>9999 & number<100000)
+    {
+        if(Math.Round(number/10000-0.5)%10==number%10)
+        {
+            if(Math.Round(number/1000-0.5)%10==Math.Round(number/10-0.5)%10)
+            {
+                Console.WriteLine("Введенное вами число является палиндромом");
+            }
+            else Console.WriteLine("Введенное вами число не является палиндромом");
+        }
+        else Console.WriteLine("Введенное вами число не является палиндромом");
+    }
+    else Console.WriteLine("Вы ввели не пятизначное число");
+}
+double Number = GetNumber();
+Palindrom(Number);
