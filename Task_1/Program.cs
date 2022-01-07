981e93335f7a3d12d6d91c3b8f644584fe5fbc36
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-07 22:37:23 +0300

    Добавил решение задачи на проверку кратности любого числа на 7 и 23

---
+++ Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+int GetNumber()
+{
+    Console.WriteLine("Введите число");
+    int Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
+}
+void Checking(int Number)
+{
+    if(Number%7==0)
+    {
+        Console.WriteLine("Введеное вами число делится на 7");
+    }
+    if(Number%23==0)
+    {
+        Console.WriteLine("Введенное вами число делится на 23");
+    }
+    if(Number%7!=0 & Number%23!=0)
+    {
+        Console.WriteLine("Введеное вами число некратно 7 и 23");
+    }
+}
+int SomeNumber = GetNumber();
+Checking(SomeNumber);
+
