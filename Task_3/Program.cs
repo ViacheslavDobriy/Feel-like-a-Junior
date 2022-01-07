b84c5e05d5dd9974218a78b2c9abf59cc0e787eb
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-07 23:19:00 +0300

    Решение определяет четверть в которой находится точка, координаты вводит пользователь

---
+++ Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число");
+    int Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
+}
+int WhatQuarter(int First, int Second)
+{
+    if(First>0)
+    {
+        if(Second>0)
+        {
+            return 1;
+        }
+        else return 4;
+    }
+    else if(Second>0) return 2;
+    else return 3;
+}
+int X = GetNumber();
+int Y = GetNumber();
+Console.WriteLine("Ваша точка находится в {0} четверти", WhatQuarter(X,Y));
