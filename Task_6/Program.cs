4f027f977c9b76bfb451e7acb351bbe90dfc9d3a
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-08 14:16:12 +0300

    удалил лишние комментарии в коде программы

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -6,24 +6,6 @@ int GetNumber()
     int Number = Convert.ToInt32(Console.ReadLine());
     return Number;
 }
-/*int GetX(int Number)
-{
-    Console.WriteLine("Введите X координату {0} точки ", Number);
-    int X = Convert.ToInt32(Console.ReadLine());
-    return X;
-}
-int GetY(int Number)
-{
-    Console.WriteLine("Введите Y координату {0} точки ", Number);
-    int Y = Convert.ToInt32(Console.ReadLine());
-    return Y;
-}
-int GetZ(int Number)
-{
-    Console.WriteLine("Введите Z координату {0} точки ", Number);
-    double Z = Convert.ToInt32(Console.ReadLine());
-    return Z;
-}*/
 Console.WriteLine("В каком пространстве будем работать?");
 Console.WriteLine("Нажмите 2 - если будем работать в 2D пространстве.");
 Console.WriteLine("Нажмите 3 - если будем работать в 3D пространстве.");
